fe4e9d69040835daf872e04729933aa5c8c20a6f
Yurii Mykhalchuk <yurkamykhalchuk@gmail.com>
2018-07-19 16:51:05 +0300

    Fourth commit

--- data_processing.py
+++ data_processing.py
@@ -1,14 +1,10 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
+
 
 def read_csv(file_name):
     return pd.read_csv("data/{0}".format(file_name))
 
-def get_pages(not_translated, translated, all_articles):
-    translated_filtered = pd.merge(translated, all_articles, how='inner', on=['title'])
-    not_translated_filtered = pd.merge(not_translated, all_articles, how='inner', on=['title'])
-
-    return translated_filtered, not_translated_filtered
 
 def filter_views(data):
     data = data.drop(columns=["Unnamed: 0", "6"])
@@ -17,71 +13,75 @@ def filter_views(data):
     data["average_views"] = average
     return data[["page_id", "average_views"]]
 
+
+def merge(translated, not_translated, other, how, left_on, right_on):
+    translated = pd.merge(left=translated,
+                          right=other,
+                          how=how,
+                          left_on=[left_on],
+                          right_on=[right_on])
+
+    not_translated = pd.merge(left=not_translated,
+                              right=other,
+                              how=how,
+                              left_on=[left_on],
+                              right_on=[right_on])
+    return translated, not_translated
+
+
 def merge_pages():
     not_translated = read_csv("uk_not_translated.csv")
     translated = read_csv("uk_translated.csv")
     all_articles = read_csv("all_uk_pages.csv")
-
-    translated_filtered, not_translated_filtered = get_pages(not_translated, translated, all_articles)
-
-    test = pd.merge(translated_filtered, not_translated_filtered, how='inner', on=['title'])
-    if test.shape[0] != 0:
-        print("Collision appeared {0} times".format(test.shape[0]))
-
     revisions_count = read_csv("revisions_count.csv")
-
-    print(translated_filtered.shape)
-    print(not_translated_filtered.shape)
-
-    translated_filtered = pd.merge(translated_filtered, revisions_count, how='inner', left_on=['id'],
-                                   right_on=['page_id'])
-    not_translated_filtered = pd.merge(not_translated_filtered, revisions_count, how='inner', left_on=['id'],
-                                       right_on=['page_id'])
-
     image_count = read_csv("number_of_imagelinks.csv")
-
-    translated_filtered = pd.merge(translated_filtered, image_count, how='left', left_on=['id'], right_on=['page_id'])
-    not_translated_filtered = pd.merge(not_translated_filtered, image_count, how='left', left_on=['id'],
-                                       right_on=['page_id'])
-
     translations_count = read_csv("number_of_translations.csv")
-
-    translated_filtered = pd.merge(translated_filtered, translations_count, how='left', left_on=['id'],
-                                   right_on=['page_id'])
-    not_translated_filtered = pd.merge(not_translated_filtered, translations_count, how='left', left_on=['id'],
-                                       right_on=['page_id'])
-
-    translated_filtered = translated_filtered.drop(['page_id_x', 'page_id_y', 'page_id'], axis=1)
-    not_translated_filtered = not_translated_filtered.drop(['page_id_x', 'page_id_y', 'page_id'], axis=1)
-    translated_filtered = translated_filtered.rename(columns={'id': 'page_id'})
-    not_translated_filtered = not_translated_filtered.rename(columns={'id': 'page_id'})
-
     first_edit = read_csv("first_edit_all_uk_pages.csv")
-    first_edit = first_edit.drop(columns= ["first_edit", "end_date"], axis=1)
-
-    not_translated_filtered = pd.merge(not_translated_filtered, first_edit, how='inner', on=['page_id'])
-    translated_filtered = pd.merge(translated_filtered, first_edit, how='inner', on=['page_id'])
-
     contributors = read_csv("contributions_by_username_or_ip_all_uk_pages.csv")
-
-    not_translated_filtered = pd.merge(not_translated_filtered, contributors, how='inner', on=['page_id'])
-    translated_filtered = pd.merge(translated_filtered, contributors, how='inner', on=['page_id'])
-
     not_translated_views = read_csv("not_translated_views_from_2018.csv")
     translated_views = read_csv("translated_views_from_2018.csv")
 
     not_translated_views = filter_views(not_translated_views)
     translated_views = filter_views(translated_views)
 
-    not_translated_filtered = pd.merge(not_translated_filtered, not_translated_views, how='inner', on=['page_id'])
-    translated_filtered = pd.merge(translated_filtered, translated_views, how='inner', on=['page_id'])
+    first_edit = first_edit.drop(columns=["first_edit", "end_date"], axis=1)
+
+    translated, not_translated = merge(translated, not_translated, all_articles, 'inner', 'title',
+                                       'title')
+
+    assert pd.merge(translated, not_translated, how='inner', on=['title']).shape[0] == 0, \
+        "Translated and not translated articles are overlapping"
+
+    translated, not_translated = merge(translated, not_translated, revisions_count, 'inner', 'id',
+                                       'page_id')
+
+    translated, not_translated = merge(translated, not_translated, image_count, 'left', 'id',
+                                       'page_id')
+
+    translated, not_translated = merge(translated, not_translated, translations_count, 'left', 'id',
+                                       'page_id')
+
+    translated = translated.drop(['page_id_x', 'page_id_y', 'page_id'], axis=1)
+    not_translated = not_translated.drop(['page_id_x', 'page_id_y', 'page_id'], axis=1)
+
+    translated = translated.rename(columns={'id': 'page_id'})
+    not_translated = not_translated.rename(columns={'id': 'page_id'})
+
+    translated, not_translated = merge(translated, not_translated, first_edit, 'inner', 'page_id',
+                                       'page_id')
+
+    translated, not_translated = merge(translated, not_translated, contributors, 'inner', 'page_id',
+                                       'page_id')
+
+    not_translated = pd.merge(not_translated, not_translated_views, how='inner', on=['page_id'])
+    translated = pd.merge(translated, translated_views, how='inner', on=['page_id'])
+
+    print(translated.shape)
+    print(not_translated.shape)
+
+    return translated, not_translated
 
 
-    print(translated_filtered.shape)
-    print(not_translated_filtered.shape)
-
-    return translated_filtered, not_translated_filtered
-
-# translated_filtered, not_translated_filtered = merge_pages()
-# translated_filtered.to_csv('data/translated_filtered.csv', index=False)
-# not_translated_filtered.to_csv('data/not_translated_filtered.csv', index=False)
+translated_filtered, not_translated_filtered = merge_pages()
+translated_filtered.to_csv('data/translated_filtered.csv', index=False)
+not_translated_filtered.to_csv('data/not_translated_filtered.csv', index=False)
